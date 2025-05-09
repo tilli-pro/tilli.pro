1f689604cd86e374a1dfc1d2524198210cc253d7
Claudia Aziz <claudiaa@tilli.pro>
2025-05-08 21:09:04 -0400

    chore: replace prettier.config.js with prettier.config.mjs and remove unused SVG files

--- nextjs-website/src/app/page.tsx
+++ nextjs-website/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Header from "@/components/Header";
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default async function IndexPage() {
-  return (
-    <main className="min-h-screen">
-      <Header />
-    </main>
-  )
+	return (
+		<main className='min-h-screen flex flex-col'>
+			<Header />
+			<Footer />
+		</main>
+	);
 }
