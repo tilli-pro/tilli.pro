d53df14e7cc3f6cd80400026ab2903a9f7d37768
Claudia Aziz <claudiaa@tilli.pro>
2025-05-12 09:51:27 -0400

    refactor(landing-page): simplify Section2 component by using a logo array for dynamic rendering of tenant logos

--- nextjs-website/src/components/LandingPage/Section2.tsx
+++ nextjs-website/src/components/LandingPage/Section2.tsx
@@ -1,38 +1,42 @@
 import Image from 'next/image';
 
+const tenantLogos = [
+	{
+		src: '/images/tenant-logos/washington-gas.png',
+		alt: 'Washington Gas',
+	},
+	{
+		src: '/images/tenant-logos/new-york-power-authority.png',
+		alt: 'New York Power Authority',
+	},
+	{
+		src: '/images/tenant-logos/howard-county.png',
+		alt: 'Howard County',
+	},
+	{
+		src: '/images/tenant-logos/anglian-water.png',
+		alt: 'Anglian Water',
+	},
+	{
+		src: '/images/tenant-logos/frontier.png',
+		alt: 'Frontier',
+	},
+	{
+		src: '/images/tenant-logos/northeast-ohio-natural-gas.png',
+		alt: 'Northeast Ohio Natural Gas',
+	},
+	{
+		src: '/images/tenant-logos/freeman.png',
+		alt: 'Freeman',
+	},
+];
+
 export default function Section2() {
 	return (
-		<div className='flex w-full gap-4 justify-between pl-4 py-8'>
-			<Image
-				className='h-20 w-auto'
-				src='/images/tenant-logos/washington-gas.png'
-				alt='Washington Gas'
-				width={200}
-				height={0}
-			/>
-			<Image
-				className='h-20 w-auto'
-				src='/images/tenant-logos/new-york-power-authority.png'
-				alt='New York Power Authority'
-				width={200}
-				height={0}
-			/>
-			<Image
-				className='h-20 w-auto'
-				src='/images/tenant-logos/howard-county.png'
-				alt='Howard County'
-				width={200}
-				height={0}
-			/>
-			<Image className='h-20 w-auto' src='/images/tenant-logos/frontier.png' alt='Frontier' width={200} height={0} />
-			<Image
-				className='h-20 w-auto'
-				src='/images/tenant-logos/northeast-ohio-natural-gas.png'
-				alt='Northeast Ohio Natural Gas'
-				width={200}
-				height={0}
-			/>
-			<Image className='h-20 w-auto' src='/images/tenant-logos/freeman.png' alt='Freeman' width={200} height={0} />
+		<div className='flex w-full gap-4 justify-between items-center pl-6 py-8'>
+			{tenantLogos.map((logo, index) => (
+				<Image key={index} className='h-18 w-auto' src={logo.src} alt={logo.alt} width={150} height={0} />
+			))}
 		</div>
 	);
 }
