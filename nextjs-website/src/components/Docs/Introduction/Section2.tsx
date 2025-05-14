273a96474a0e1a8fbb49d06d2538533ab171961f
Claudia Aziz <claudiaa@tilli.pro>
2025-05-14 12:58:55 -0400

    refactor(docs): update Section2 layout with improved spacing and styling adjustments

--- nextjs-website/src/components/Docs/Introduction/Section2.tsx
+++ nextjs-website/src/components/Docs/Introduction/Section2.tsx
@@ -2,19 +2,18 @@ import { ArrowRightIcon } from 'lucide-react';
 
 export default function Section2() {
 	return (
-		<div className='text-black py-20 px-10'>
+		<div className='text-black py-20 px-10 space-y-10'>
 			<h2 className='text-5xl'>tilli Documentation</h2>
 
-			<div className='flex gap-4'>
+			<div className='flex gap-4 justify-between'>
 				{/* 3 mini cards/links */}
-				<div className='flex-1 bg-[#FCFCFC] group flex items-center p-4 rounded-lg hover:border-[#448DE6] transition-colors cursor-pointer'>
+				<div className='w-72 bg-gradient-button-shadow border border-foreground-muted group flex items-center p-6 rounded-lg hover:border-[#448DE6] transition-colors cursor-pointer'>
 					<div>ℹ️</div>
-					<span className='ml-2 flex-grow'>Users Guide</span>
-					{/* Only visible when hovered over the card */}
+					<span className='ml-4'>Users Guide</span>
 					<ArrowRightIcon className='size-4 opacity-0 group-hover:opacity-100 text-[#448DE6] transition-opacity' />
 				</div>
-				<div className='flex-1'></div>
-				<div className='flex-1'></div>
+				<div className='flex-1 w-72'></div>
+				<div className='flex-1 w-72'></div>
 			</div>
 		</div>
 	);
