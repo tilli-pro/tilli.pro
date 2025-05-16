e1645d11e6ca1f414d2ce93cb3d30d06c87ac67d
Ibrahim Saberi <ibrahims@tilli.pro>
2025-05-16 12:52:13 -0400

    fix: fonts missing from docs router group

--- nextjs-website/src/app/(docs)/docs/layout.tsx
+++ nextjs-website/src/app/(docs)/docs/layout.tsx
@@ -1,12 +1,29 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { DocsSidebar } from '@/components/Docs/Sidebar';
+import { Sora, Inter } from 'next/font/google';
+import { Metadata } from 'next';
 import DocsHeader from '@/components/Docs/Header';
 import "../../globals.css";
+
+const sora = Sora({
+	variable: '--font-sora',
+	subsets: ['latin'],
+});
+
+const inter = Inter({
+	variable: '--font-inter',
+	subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+	title: "tilli Developers",
+	description: "tilli Developers Documentation",
+};
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html>
-			<body>
+			<body className={`${sora.variable} ${inter.variable} antialiased`}>
 		<SidebarProvider>
 			<div className='flex flex-col min-h-screen w-full'>
 				<DocsHeader />
