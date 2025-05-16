25dc1210ca06b46d788f27d5075c90a2f4ddce0c
Claudia Aziz <claudiaa@tilli.pro>
2025-05-16 15:12:38 -0400

    chore: remove vulnerables

--- nextjs-website/src/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import {createClient} from 'next-sanity'
-
-export const client = createClient({
-  projectId: 'g7u8cmw2',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: false,
-})
