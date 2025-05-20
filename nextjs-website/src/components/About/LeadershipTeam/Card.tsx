394a646c9e0a8a5b5d8e2b346b2af4c6e17cebfc
Claudia Aziz <claudiaa@tilli.pro>
2025-05-20 12:53:14 -0400

    style: improve LeadershipTeam component layout and responsiveness, update button styles, and add hover effect for LinkedIn button

--- nextjs-website/src/components/About/LeadershipTeam/Card.tsx
+++ nextjs-website/src/components/About/LeadershipTeam/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 export default function Card({ name, title, image }: CardProps) {
 	return (
 		<div
-			className='flex flex-col items-center rounded-xl border-[6px] border-background 
+			className='h-full flex flex-col items-center rounded-xl border-[6px] border-background 
             px-6 py-8 text-center shadow-[inset_0_0_0_1px_#BDBDBD] 
             transition-all duration-300 ease-in-out hover:border-pink-200 hover:shadow-none 
             [background:linear-gradient(180deg,rgba(244,244,244,0.5)_5.77%,#FFF_31.73%,#FFF_100%)]'
