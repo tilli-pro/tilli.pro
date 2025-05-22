12e325a534b1d72382de3a9fe26c13fc9136b144
Claudia Aziz <claudiaa@tilli.pro>
2025-05-22 14:09:52 -0400

    style: add new gradient for card background in globals.css and replace B2C payment solutions section with PaymentSolutionsSplitSection in TilliPay page

---
+++ nextjs-website/src/components/tilliPay/PaymentSolutionsSplitSection/DigitalPaymentsHighlight.tsx
@@ -0,0 +1,26 @@
+import { BellRingIcon } from 'lucide-react';
+import Image from 'next/image';
+
+export default function DigitalPaymentsHighlight() {
+	return (
+		<div className='relative border [background-image:var(--gradient-card-bg)] border-b-figma-gray-light rounded-lg p-6 w-2/5 space-y-6 overflow-hidden'>
+			<h3 className='text-2xl'>An Online Payment Platform for the New Age</h3>
+			<p>
+				Accept multiple payment methods, create offers, and seamlessly onboard customers — tilliPay’s Digital Payment
+				Solution has it all.
+			</p>
+
+			<div className='bg-[#325ef6]/20 rounded-lg p-2 w-fit ml-10'>
+				<BellRingIcon className='size-6 text-[#325EF6]' strokeWidth={1.5} />
+			</div>
+
+			<Image
+				src='/images/invoice-view1.png'
+				alt='invoice view'
+				width={1000}
+				height={1000}
+				className='absolute -bottom-8 -right-35'
+			/>
+		</div>
+	);
+}
